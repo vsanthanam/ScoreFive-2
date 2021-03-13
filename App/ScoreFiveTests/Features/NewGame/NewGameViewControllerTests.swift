8675b419b41d8e44f0f3080b51346020aa58ef53
Varun Santhanam <talkto@vsanthanam.com>
2021-03-13 15:36:56 -0800

    [NewGame] Add NewGameViewControllerTests
    Summary:
    Add empty testcase for `NewGameViewController`
    
    This diff mostly exists to do an e2e test of the new tuist + buildkite ci pipline
    
    Test Plan: n/a
    
    Reviewers: O3 ScoreFive - iOS Core, vsanthanam
    
    Reviewed By: O3 ScoreFive - iOS Core, vsanthanam
    
    Differential Revision: https://phab.vsanthanam.com/D200

---
+++ App/ScoreFiveTests/Features/NewGame/NewGameViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  NewGameViewControllerTests.swift
+//  ScoreFiveTests
+//
+//  Created by Varun Santhanam on 3/13/21.
+//  Copyright © 2021 Varun Santhanam. All rights reserved.
+//
+
+import Foundation
+@testable import ScoreFive
+import XCTest
+
+final class NewGameViewControllerTests: TestCase {
+    
+    let listener = NewGamePresentableListenerMock()
+    
+    var viewController: NewGameViewController!
+    
+    override func setUp() {
+        super.setUp()
+        viewController = .init()
+        viewController.listener = listener
+    }
+    
+}
