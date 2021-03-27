55cfd57bdf6f315e289fabb53f999ef0f5d7ccc4
Varun Santhanam <talkto@vsanthanam.com>
2021-03-27 14:42:44 -0700

    [Lint] Undo Tuist Project Linting
    Summary: Should really be modeled as a test
    
    Test Plan: n/a
    
    Reviewers: O9 ScoreFive - Tooling, vsanthanam
    
    Reviewed By: O9 ScoreFive - Tooling, vsanthanam
    
    Differential Revision: https://phab.vsanthanam.com/D236

--- Tooling/sftool/Sources/sftool/Commands/DevelopCommand.swift
+++ Tooling/sftool/Sources/sftool/Commands/DevelopCommand.swift
@@ -26,24 +26,11 @@ struct DevelopCommand: ParsableCommand {
     @Flag(name: .shortAndLong, help: "Don't automatically open Xcode")
     var dontOpenXcode: Bool = false
 
-    @Option(name: .shortAndLong, help: "The file to check")
-    var fileCondition: String?
-
     func run() throws {
-        if let fileCondition = fileCondition {
-            if fileCondition.contains("Project.swift") || fileCondition.contains("Workspace.swift") {
-                do {
-                    try Commands.generate(on: root)
-                } catch {
-                    print("Error:1 Workspace cannot be generated!")
-                }
-            }
-        } else {
-            try? Commands.killXcode()
-            try Commands.generate(on: root)
-            if !dontOpenXcode {
-                try Commands.openWorkspace(on: root)
-            }
+        try? Commands.killXcode()
+        try Commands.generate(on: root)
+        if !dontOpenXcode {
+            try Commands.openWorkspace(on: root)
         }
     }
 }
