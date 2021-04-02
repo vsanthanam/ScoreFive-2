000483d116ac98989a1ca7609e7c851fe456bd3e
Varun Santhanam <talkto@vsanthanam.com>
2021-04-02 11:59:19 -0700

    [Arc] Update TuistGenTestEngine
    Summary: Update tuist unig test engine
    
    Test Plan: n/a
    
    Reviewers: vsanthanam
    
    Reviewed By: vsanthanam
    
    Differential Revision: https://phab.vsanthanam.com/D249

--- tooling/arctools/src/TuistGenTestEngine.php
+++ tooling/arctools/src/TuistGenTestEngine.php
@@ -17,45 +17,27 @@ final class TuistGenTestEngine extends ArcanistUnitTestEngine {
         $results = array();
         $this->init();
         $this->console->writeOut("  %s %s",
-            phutil_console_format('<bg:yellow>** RUN **</bg>'),
-            $this->result->getName()
+          phutil_console_format('<bg:yellow>** RUN **</bg>'),
+          $this->result->getName()
         );
-        $this->execute();
+        try {
+            list($stdout, $stderr) = execx(`./sftool develop --arcunit`);
+            $this->result->setResult(ArcanistUnitTestResult::RESULT_PASS);
+            $this->result->setName("Check Manifest Sanity");
+            $this->console->writeOut("\r%s", $this->renderer->renderUnitResult($this->result));
+        } catch(CommandException $exc) {
+            $this->result->setUserdata($exc->getStdout() . "\n" . $exc->getStderr());
+            $this->result->setResult(ArcanistUnitTestResult::RESULT_FAIL);
+            $this->result->setName("Check Manifest Sanity");
+            $this->console->writeOut("\r%s", $this->renderer->renderUnitResult($this->result));
+        }
 
         $results[] = $this->result;
         return $results;
     }
-
-    private function saveAndEchoResultCode($result_code) {
-        $this->result->setResult($result_code);
-        $this->result->setName("Tuist Sanity");
-        $this->renderer->renderUnitResult($this->result);
-        // $this->console->writeOut("\r%s", $this->renderer->renderUnitResult($this->result));
-    }
-
-    private function execute() {
-        $future = new ExecFuture("./sftool develop -d");
-        $future->setTimeout(500);
-        // $future->setEnv($this->env);
-        $this->executeFuture($future);
-    }
-
-    private function executeFuture($future) {
-        try {
-            list($stdout, $stderr) = $future->resolvex();
-            $this->saveAndEchoResultCode(ArcanistUnitTestResult::RESULT_PASS);
-        } catch(CommandException $exc) {
-            if ($future->getWasKilledByTimeout()) {
-                $this->result->setUserData(
-                    "Process stdout:\n" . $exc->getStdout() .
-                    "\nProcess stderr:\n" . $exc->getStderr() .
-                    "\nExceeded timeout of " . $this->timeout . " secs"
-                );
-            } else {
-                $this->result->setUserdata($exc->getStdout() . "\n" . $exc->getStderr());
-            }
-            $this->saveAndEchoResultCode(ArcanistUnitTestResult::RESULT_FAIL);
-        }
+    
+    public function shouldEchoTestResults() {
+        return false;
     }
 }
 
--- tooling/sftool/Sources/sftool/Commands/DevelopCommand.swift
+++ tooling/sftool/Sources/sftool/Commands/DevelopCommand.swift
@@ -24,11 +24,16 @@ struct DevelopCommand: ParsableCommand {
     @Flag(name: .shortAndLong, help: "Don't automatically open Xcode")
     var dontOpenXcode: Bool = false
 
+    @Flag(name: .shortAndLong, help: "Used by arcanist")
+    var arcunit: Bool = false
+
     func run() throws {
-        try? Commands.killXcode()
+        if !arcunit {
+            try? Commands.killXcode()
+        }
         let config = try fetchConfiguration(on: root)
         try Commands.generate(on: root, tuistRoot: config.tuistRoot)
-        if !dontOpenXcode {
+        if !dontOpenXcode, !arcunit {
             try Commands.openWorkspace(on: root, tuistRoot: config.tuistRoot)
         }
     }
