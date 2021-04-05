2575964ce6993dc157bd5e01e5426fd3820572e0
Varun Santhanam <talkto@vsanthanam.com>
2021-04-05 12:30:35 -0700

    [CI] Add clean steps to ci
    Summary: add cleanup steps to ci, add licenses for third party tooling
    
    Test Plan: n/a
    
    Reviewers: vsanthanam
    
    Reviewed By: vsanthanam
    
    Differential Revision: https://phab.vsanthanam.com/D255

--- tooling/sftool/Sources/sftool/Commands/GenerateCICommand.swift
+++ tooling/sftool/Sources/sftool/Commands/GenerateCICommand.swift
@@ -20,6 +20,12 @@ struct GenerateCICommand: ParsableCommand {
     @Flag(name: .shortAndLong, help: "Use pretty results")
     var pretty: Bool = false
 
+    @Flag(name: .long, help: "Allow lint failures")
+    var relaxed: Bool = false
+
+    @Flag(name: .long, help: "Clean repo automatically")
+    var autoclean: Bool = false
+
     // MARK: - ParsableCommand
 
     static let configuration = CommandConfiguration(commandName: "ci",
@@ -29,29 +35,57 @@ struct GenerateCICommand: ParsableCommand {
         let configuration = try fetchConfiguration(on: root)
         let device = configuration.testConfig.device
         let os = configuration.testConfig.os
-        let script: String
+        var script: String
         if pretty {
-            script = """
-            #! /bin/sh
-            set -euo pipefail
-            ./sftool lint --test
-            ./sftool analytics wipe
-            ./sftool gen deps
-            ./sftool gen mocks
-            ./sftool develop -d
-            xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test | tee -a build.log | xcpretty -c
-            """
+            if relaxed {
+                script = """
+                #! /bin/sh
+                set -euo pipefail
+                ./sftool analytics wipe
+                ./sftool gen deps
+                ./sftool gen mocks
+                ./sftool develop -d
+                xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test | tee -a build.log | xcpretty -c
+                """
+            } else {
+                script = """
+                #! /bin/sh
+                set -euo pipefail
+                ./sftool lint --test
+                ./sftool analytics wipe
+                ./sftool gen deps
+                ./sftool gen mocks
+                ./sftool develop -d
+                xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test | tee -a build.log | xcpretty -c
+                """
+            }
         } else {
-            script = """
-            #! /bin/sh
-            set -euo pipefail
-            ./sftool lint --test
-            ./sftool analytics wipe
-            ./sftool gen deps
-            ./sftool gen mocks
-            ./sftool develop -d
-            xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test
-            """
+            if relaxed {
+                script = """
+                #! /bin/sh
+                set -euo pipefail
+                ./sftool analytics wipe
+                ./sftool gen deps
+                ./sftool gen mocks
+                ./sftool develop -d
+                xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test
+                """
+            } else {
+                script = """
+                #! /bin/sh
+                set -euo pipefail
+                ./sftool lint --test
+                ./sftool analytics wipe
+                ./sftool gen deps
+                ./sftool gen mocks
+                ./sftool develop -d
+                xcodebuild -workspace \(configuration.tuist.root)/ScoreFive.xcworkspace -sdk iphonesimulator -scheme ScoreFive -destination 'platform=iOS Simulator,name=\(device),OS=\(os)' test
+                """
+            }
+
+        }
+        if autoclean {
+            script += "\n./sftool clean"
         }
         print(script)
     }
